979844870dce76f27bfe558a6f063efd2a12ac57
Abir Ayari <abir.ayari@esprit.tn>
2023-10-21 13:06:14 +0100

    Revert "Mockito CRUD without delete commit" "get my code beck"
    This reverts commit 02028540fb9c9c8833713b54231f7d0a27afd713.

--- src/test/java/tn/esprit/spring/kaddem/EtudiantServiceImpMock.java
+++ src/test/java/tn/esprit/spring/kaddem/EtudiantServiceImpMock.java
@@ -6,23 +6,20 @@ import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
-import static org.mockito.Mockito.times;
 import org.mockito.junit.MockitoJUnitRunner;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 
 
-import org.springframework.test.web.client.ExpectedCount;
 import tn.esprit.spring.kaddem.entities.Etudiant;
 import tn.esprit.spring.kaddem.repositories.EtudiantRepository;
 import tn.esprit.spring.kaddem.services.EtudiantServiceImpl;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
-import static org.junit.Assert.assertEquals;
 import static org.springframework.test.web.client.ExpectedCount.times;
-import static tn.esprit.spring.kaddem.entities.Option.SIM;
 
 @SpringBootTest
 @RunWith(MockitoJUnitRunner.class)
@@ -33,14 +30,13 @@ public class EtudiantServiceImpMock {
     @InjectMocks
     EtudiantServiceImpl etudiantService;
 
-    Etudiant e1 = new Etudiant(1,"Ayari", "Abir",SIM);
-    Etudiant e2 = new Etudiant(2,"Boussema", "Ahmed",SIM);
+    Etudiant e1 = new Etudiant("Ayari", "Abir");
+    Etudiant e2 = new Etudiant("Boussema", "Ahmed");
 
     List<Etudiant> listEtudiants = new ArrayList<Etudiant>() {{
         add(e1);
-        add(e2);
-        add(new Etudiant("Marzouki", "Rym"));
-        add(new Etudiant("Ben Khalil", "Cyrine"));
+        add(new Etudiant("XX", "XXX"));
+        add(new Etudiant("eXX", "XXeeeX"));
     }};
 
     @Test
@@ -50,36 +46,4 @@ public class EtudiantServiceImpMock {
         //assertNotNull(produit1);
         Mockito.verify(etudiantRepository).save(Mockito.any(Etudiant.class));
     }
-
-    @Test
-    public void testretrieveAllEtudiants() {
-        Mockito.when(etudiantRepository.findAll()).thenReturn(listEtudiants);
-        List<Etudiant> listEtudiants1 = etudiantService.retrieveAllEtudiants();
-        assertEquals(listEtudiants.size(), listEtudiants1.size());
-
-    }
-    @Test
-    public void testupdateEtudiant(){
-        Mockito.when(etudiantRepository.save(e2)).thenReturn(e2);
-        Etudiant etudiant1 = etudiantService.updateEtudiant(e2);
-        Mockito.verify(etudiantRepository).save(Mockito.any(Etudiant.class));
-        //Mockito.verify(etudiantRepository,times(1)).save(Mockito.any(Etudiant.class));
-
-    }
-
-   /* @Test
-    public void testdeleteEtudiant(){
-        etudiantService.removeEtudiant(2);
-        Mockito.verify(etudiantRepository).deleteById(2);
-    }
-*/
-
-
-
-
-
-
-
-
-
 }
